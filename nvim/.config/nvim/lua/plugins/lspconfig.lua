1185089775c431df0692563062491df6daaa75da
Jason Hilder <jason@pop-os.localdomain>
2022-04-15 18:46:19 +0200

    added golang server to lsp.

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -117,6 +117,26 @@ local function setup_servers()
         end
     end
 
+    local go_server, requested_server = lsp_installer_servers.get_server("gopls")
+    if go_server then
+        requested_server:on_ready(function ()
+            local opts = {
+                on_attach = function(client, bufnr)
+                    require "lsp_signature".on_attach({
+                        use_lspsaga = true,
+                        hint_enable = true,
+                        floating_window = false,
+                    }, bufnr)
+                end,
+            }
+            requested_server:setup(opts)
+        end)
+        if not requested_server:is_installed() then
+            -- Queue the server to be installed
+            requested_server:install()
+        end
+    end
+
     local ts_server, requested_server = lsp_installer_servers.get_server("tsserver")
     if ts_server then
         requested_server:on_ready(function ()
