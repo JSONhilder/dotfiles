0b37c05fe81fda0882d0d58858c3ed479e0827c5
jason hilder <jhilder95@gmail.com>
2022-11-12 17:55:03 +0200

    removed old nvim config.

--- nvim_old/.config/nvim/lua/utils.lua
+++
@@ -1,8 +0,0 @@
--- hide line numbers , statusline in specific buffers!
-vim.api.nvim_exec([[
-   au TermOpen term://* setlocal nonumber laststatus=2
-   au TermClose term://* setlocal laststatus=2
-]], false)
-
---we can load shada now
-vim.opt.shadafile = ""
