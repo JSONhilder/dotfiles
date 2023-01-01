d5d50b9eff3038a51a831d1aaa303d45e4761080
jason hilder <jhilder95@gmail.com>
2023-01-01 15:38:43 +0200

    scripts cleanup.

--- scripts/.local/bin/compile.sh
+++ scripts/.local/bin/compile.sh
@@ -8,7 +8,11 @@ done
 # Check if rust project
 FILE=./Cargo.toml
 if test -f "$FILE"; then
-    cargo run $ARGS
-else
-    echo "not a rust project"
+    alacritty --hold -e cargo run $ARGS
+fi
+
+# Check if node project
+FILE=./package.json
+if test -f "$FILE"; then
+    alacritty --hold -e node $ARGS
 fi
