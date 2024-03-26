1ac1315797ed886a66c595c99c48533840b25fbc
Jason Hilder <jhilder95@gmail.com>
2024-03-26 18:30:42 +0200

    Added telescope to nvim.

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+        local telescope = require("telescope")
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        telescope.setup({
+            pickers = {
+                live_grep = {
+                    file_ignore_patterns = { 'node_modules', '.git', '.venv', 'vendor'},
+                    additional_args = function(_)
+                        return { "--hidden" }
+                    end
+                },
+                find_files = {
+                    file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+                    hidden = true
+                }
+
+            },
+        })
+    end
+}
