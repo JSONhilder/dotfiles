b58b48cfe39bc200ae39e2fa1a14e0e18a965cc5
Jason Hilder <jhilder95@gmail.com>
2024-10-08 08:22:29 +0200

    Updated system install.

--- system_install/system_install.sh
+++ system_install/system_install.sh
@@ -24,13 +24,13 @@ fi
 #####################################################
 # CREATE DIRECTORIES IF NOT EXIST
 #####################################################
-mkdir -p ~/.fonts ~/github ~/tests
+mkdir -p ~/.fonts ~/github 
 #####################################################
 # SYSTEM ESSENTIALS
 #####################################################
 if [[ $_override = "n" ]]; then
     echo "Install system essentials? [y/n]"
-    echo "( gnome-tweaks, git, vim, make, gcc, curl, ripgrep, gnu stow, xclip, unzip )"
+    echo "( git, vim, make, gcc, curl, wget, ripgrep, xclip, unzip, tree, btop, neofetch )"
     read _proceed
     # lowercase it
     _proceed=${_proceed,,}
@@ -41,7 +41,7 @@ if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
     echo "installing essentials..."
     echo "------------------------"
     echo
-    sudo apt install -y gnome-tweaks vim-runtime vim-gui-common make gcc curl ripgrep xclip stow unzip build-essential
+    sudo apt install -y git make gcc curl wget ripgrep xclip unzip build-essential tree btop neofetch
     echo
     echo "===== COMPLETE ====="
     echo
@@ -49,11 +49,10 @@ else
     echo
 fi
 #####################################################
-# SYSTEM NICETIES
+# Fish Shell
 #####################################################
 if [[ $_override = "n" ]]; then
-    echo "Install system niceties ? [y/n]"
-    echo "( tree, neofetch, btop )"
+    echo "Install fish shell ? [y/n]"
     read _proceed
     # lowercase it
     _proceed=${_proceed,,}
@@ -61,32 +60,10 @@ fi
 
 if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
     echo ""
-    echo "installing system niceties..."
-    echo "-----------------------------"
-    echo
-    sudo apt install -y tree neofetch btop
-    echo
-    echo "===== COMPLETE ====="
-    echo
-else
-    echo
-fi
-#####################################################
-# ZSH (Z-SHELL)
-#####################################################
-if [[ $_override = "n" ]]; then
-    echo "Install zsh (z-shell) ? [y/n]"
-    read _proceed
-    # lowercase it
-    _proceed=${_proceed,,}
-fi
-
-if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
-    echo ""
-    echo "installing zsh..."
+    echo "installing fish..."
     echo "-----------------"
     echo
-    sudo apt install -y zsh
+    sudo apt install -y fish
     echo
     echo "===== COMPLETE ====="
     echo
@@ -94,41 +71,10 @@ else
     echo
 fi
 #####################################################
-# OH MY ZSH
+# LAZYGIT
 #####################################################
 if [[ $_override = "n" ]]; then
-    echo "Install oh my zsh? [y/n]"
-    read _proceed
-    # lowercase it
-    _proceed=${_proceed,,}
-fi
-
-if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
-    echo installing oh my zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-    #OH MY ZSH PLUGINS
-    if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
-        echo "installing zsh auto suggestion plugin..."
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    fi
-
-    if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]]; then
-        echo "installing zsh syntax highlighting plugin..."
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    fi
-
-    echo
-    echo "===== COMPLETE ====="
-    echo
-else
-    echo
-fi
-#####################################################
-# STARSHIP
-#####################################################
-if [[ $_override = "n" ]]; then
-    echo "Install Starship? [y/n]"
+    echo "Install Lazy Git? [y/n]"
     read _proceed
     # lowercase it
     _proceed=${_proceed,,}
@@ -136,11 +82,26 @@ fi
 
 if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
     echo ""
-    echo "Installing starship..."
-    echo "------------------"
+    echo "Installing lazygit..."
+    echo "---------------------"
     echo
 
-    curl -sS https://starship.rs/install.sh | sh
+    tag=$(curl --silent https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+
+    echo $tag
+
+    VERSION=$(echo $tag|cut -c 2-7)
+    FILE="lazygit_"$VERSION"_Linux_x86_64.tar.gz"
+
+    
+
+    download="https://github.com/jesseduffield/lazygit/releases/download/"$tag"/"$FILE
+    cd ~ && wget -O $FILE $download
+
+    tar xvf $FILE lazygit
+    sudo mv lazygit /usr/local/bin/lazygit
+    rm $FILE
+
     echo
     echo "===== COMPLETE ====="
     echo
@@ -181,110 +142,6 @@ if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
     sudo apt update
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
     sudo usermod -aG docker $USER
-
-    echo
-    echo "===== COMPLETE ====="
-    echo
-else
-    echo
-fi
-#####################################################
-# VS CODE
-#####################################################
-if [[ $_override = "n" ]]; then
-    echo "Install Visual Studio Code? [y/n]"
-    read _proceed
-    # lowercase it
-    _proceed=${_proceed,,}
-fi
-
-if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
-    echo ""
-    echo "installing Visual Studio Code..."
-    echo "---------------------------------"
-    echo
-
-    download="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-    cd ~ && wget -O "vscode.deb" $download
-
-    sudo apt install "./vscode.deb"
-    rm vscode.deb
-
-    echo
-    echo
-    echo "===== COMPLETE ====="
-    echo
-else
-    echo
-fi
- echo
-#####################################################
-# ZOLA STATIC SITE GENERATOR
-#####################################################
-if [[ $_override = "n" ]]; then
-    echo "Install ZOLA static site generator? [y/n]"
-    read _proceed
-    # lowercase it
-    _proceed=${_proceed,,}
-fi
-
-if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
-    echo ""
-    echo "Installing zola..."
-    echo "------------------"
-    echo
-
-    tag=$(curl --silent https://api.github.com/repos/getzola/zola/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-
-    echo $tag
-
-    FILE="zola-"$tag"-x86_64-unknown-linux-gnu.tar.gz"
-    echo $FILE
-
-    download="https://github.com/getzola/zola/releases/download/"$tag"/"$FILE
-    cd ~ && wget -O $FILE $download
-
-    tar xvf $FILE zola
-    sudo mv zola /usr/local/bin/
-    rm $FILE
-
-    echo
-    echo "===== COMPLETE ====="
-    echo
-else
-    echo
-fi
-#####################################################
-# LAZYGIT
-#####################################################
-if [[ $_override = "n" ]]; then
-    echo "Install Lazy Git? [y/n]"
-    read _proceed
-    # lowercase it
-    _proceed=${_proceed,,}
-fi
-
-if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
-    echo ""
-    echo "Installing lazygit..."
-    echo "---------------------"
-    echo
-
-    tag=$(curl --silent https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-
-    echo $tag
-
-    VERSION=$(echo $tag|cut -c 2-7)
-    FILE="lazygit_"$VERSION"_Linux_x86_64.tar.gz"
-
-    
-
-    download="https://github.com/jesseduffield/lazygit/releases/download/"$tag"/"$FILE
-    cd ~ && wget -O $FILE $download
-
-    tar xvf $FILE lazygit
-    sudo mv lazygit /usr/local/bin/lazygit
-    rm $FILE
 
     echo
     echo "===== COMPLETE ====="
@@ -392,33 +249,8 @@ if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
     echo "------------------------------"
     echo
 
-    chsh -s $(which zsh)
+    chsh -s $(which fish)
 
-    echo
-    echo "===== COMPLETE ====="
-    echo
-else
-    echo
-fi
-#####################################################
-# Symlink dot files
-#####################################################
-if [[ $_override = "n" ]]; then
-    echo "Stow(symlink) dotfiles? [y/n]"
-    read _proceed
-    # lowercase it
-    _proceed=${_proceed,,}
-fi
-
-if [[ $_proceed = "y" ]] || [[ $_proceed = "yes" ]]; then
-    echo ""
-    echo "Symlinking dotfiles..."
-    echo "----------------------"
-    echo
-    cd ~/dotfiles/
-    sudo rm -rf ~/.vimrc && stow vim
-    sudo rm -rf ~/.zshrc && stow zsh
-    sudo rm -rf ~/.config/starship.toml && stow starship
     echo
     echo "===== COMPLETE ====="
     echo
