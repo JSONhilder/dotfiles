1e3b60d5135869ef6d34293b1ad3afbf40a65ad0
Jason Hilder <jhilder95@gmail.com>
2024-10-17 07:15:08 +0200

    Updated emacs config.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -197,26 +197,12 @@
     :config
     (global-evil-surround-mode 1))
 
-;; @TODO: show dotfiles
-(use-package fzf
-  :ensure t
-  :bind
-    ;; Don't forget to set keybinds!
-  :config
-  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
-        fzf/executable "fzf"
-        fzf/git-grep-args "-i --line-number %s"
-        ;; command used for `fzf-grep-*` functions
-        ;; example usage for ripgrep:
-        ;; fzf/grep-command "rg --no-heading -nH"
-        fzf/grep-command "grep -nrH"
-        ;; If nil, the fzf buffer will appear at the top of the window
-        fzf/position-bottom t
-        fzf/window-height 15))
-
 ;; An extremely feature-rich git client. Activate it with "C-c g".
 (use-package magit
     :ensure t)
+
+(with-eval-after-load 'magit
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
 
 (use-package vterm
     :ensure t
@@ -293,10 +279,20 @@
     (interactive)
     (switch-to-buffer nil))
 
+(defun google-highlighted-region ()
+  "Search LibreWolf using the currently highlighted region in Emacs."
+  (interactive)
+  (if (use-region-p)
+      (let ((query (buffer-substring-no-properties (region-beginning) (region-end))))
+        (let ((url (concat "https://www.google.com/search?q=" (url-hexify-string query))))
+          (start-process "librewolf" nil "librewolf" url)))
+    (message "No region selected.")))
+
 ;; MISC BINDS
 (with-eval-after-load 'dired
   (evil-define-key 'normal dired-mode-map (kbd "f") 'find-file))
 
+(global-set-key (kbd "C-c s") 'google-highlighted-region)
 
 ;; EVIL BINDS
 (with-eval-after-load 'evil
